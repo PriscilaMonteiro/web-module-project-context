6d80523780359c42365b77abffd08c169e168476
Priscila Ribeiro Monteiro <pririmonteiro@Priscilas-MBP.attlocal.net>
2021-10-07 18:14:22 -0700

    mvp completed

--- src/App.js
+++ src/App.js
@@ -2,31 +2,44 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+import ProductContext from './contexts/productContext';
+
+import CartContext from './contexts/cartContext';
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
 function App() {
+	const { Provider } = ProductContext;
+
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item]);
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<CartContext.Provider value={{ cart }}>
+			<Navigation />
 
 			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+			
+				<Provider value={{ products, addItem }}>
+					<Route exact path="/">
+						<Products />
+					</Route>
+				</Provider>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+				<Route path="/cart">
+					<ShoppingCart />
+				</Route>
+			
+			</CartContext.Provider>
 		</div>
 	);
 }
---
+++ src/contexts/productContext.js
@@ -0,0 +1,8 @@
+import { createContext } from "react";
+
+const ProductContext = createContext()
+
+export default ProductContext
+
+// when creating context it is better not to capitalize the name of the file to be clear that it is not a component file.
+
